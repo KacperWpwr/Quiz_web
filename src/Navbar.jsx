8137db4b1ad58169701f32f866ecc0ec3aabc27d
kacper <kacper.a.wieczorek1@gmail.com>
2023-02-16 15:41:26 +0100

    Updated navbar: -added search bar -added button container

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,7 +4,14 @@ export default function Navbar({page,setPage}){
     return (
         <div className="navbar">
             <div className="page_name" onClick={()=>{setPage(Page.MainPage)}}>QUIZ</div>
+            <div  className="search-bar-container">
+                <input type="text" className="search-bar"></input>
+                <div className="search-bar-icon"><i className="fa fa-search"></i></div>
+            </div>
 
+            <div className="navbar-button-container">
+                <div className="navbar-button">Login/Register</div>
+            </div>
         </div>
     )
 }
